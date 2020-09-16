2d967f2df1029768d16d3be94a3ab102d6b2c1aa
Grant Miller <grant@grant-miller.com>
2020-09-16 13:35:51 -0400

    example of how to use the API

---
+++ main.py
@@ -0,0 +1,48 @@
+'''
+First you will need to setup an account and add a service account and room.
+The use the API KEY found on this page: https://qr-room.grant-miller.com/settings in your request body
+
+'''
+
+import requests
+import config
+
+s = requests.session()
+
+# Get all the rooms for your account
+resp = s.get(
+    url='http://192.168.68.105:5000/api/get_rooms',
+    data={'apiKey': config.APIKEY},  # get your API key from https://qr-room.grant-miller.com/settings
+)
+print('resp.text=', resp.text)
+print('resp.json=', resp.json())
+
+rooms = resp.json()
+
+# Get the "book now" url for each room
+# opening this url will ask the user to login with their Microsoft account and then book the room
+for room in rooms:
+    resp = s.get(
+        url='http://192.168.68.105:5000/api/get_book_now_url',
+        data={
+            'apiKey': config.APIKEY,  # get your API key from https://qr-room.grant-miller.com/settings
+            'roomID': room['id'],
+        },
+    )
+
+    url = resp.json()
+    print('room=', room, ', url=', url)
+
+# Get the "book now" QR code for each room, save it to a file
+# opening this QR/url will ask the user to login with their Microsoft account and then book the room
+# some QRs are one-time-use only. This is indicated by the room['dynamicQR'] property
+for room in rooms:
+    resp = s.get(
+        url='http://192.168.68.105:5000/api/get_book_now_qr',
+        data={
+            'apiKey': config.APIKEY,  # get your API key from https://qr-room.grant-miller.com/settings
+            'roomID': room['id'],
+        },
+    )
+    with open(f'qr_{room["email"]}.png', mode='wb') as file:
+        file.write(resp.content)
